c6d8d902a3339d917c080b50332da24fd912f0da
Anton Okhrimenko <antony.okhrimenko@gmail.com>
2024-07-07 19:55:29 +0200

    1st commit main branch

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Первый пуш в GitHub основного проекта");
+
+        System.out.println("Создали ветку, ответвились от основной. Внесли изменения");
+        System.out.println("Делаем коммит и пул-реквест");
     }
 }
